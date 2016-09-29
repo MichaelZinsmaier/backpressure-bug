b44c48c13e25f7b55965ae3c2363125dd43fd6da
Michael Zinsmaier <michael.zinsmaier@atos.net>
2016-09-29 15:05:33 +0200

    Reproduced probably with simplified test
    Without the counters, other overhead. Simply backpressuring in the
    serverlayer should propagate to the source. Instead the source can
    finish and subsequently tears down the stream.
    
    (Tear down should happen anyway but earliest after 50 seconds delay,
    since not taking any elements in the server should backpressure the
    source and thus avoid completion for the first 50 seconds)

--- src/test/scala/ws/backpressuredemo/ServerLayerTest.scala
+++ src/test/scala/ws/backpressuredemo/ServerLayerTest.scala
@@ -1,39 +1,19 @@
 package ws.backpressuredemo
 
-import java.util.concurrent.atomic.AtomicInteger
-
 import scala.concurrent.Await
 import scala.concurrent.Future
-
-import akka.actor.ActorSystem
-import akka.testkit.TestKit
-import akka.util.ByteString
-import akka.util.ByteStringBuilder
-
-import org.scalatest.FlatSpecLike
-import org.scalatest.Matchers
-import org.scalatest.Suite
-import org.testng.annotations.Test
 import scala.concurrent.duration.DurationInt
-import scala.util.Success
 
 import akka.NotUsed
-import akka.http.scaladsl.model.ws.WebSocketRequest
+import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.ContentType
-import akka.http.scaladsl.model.ContentTypes
-import akka.http.scaladsl.model.HttpEntity
-import akka.http.scaladsl.model.HttpMethods
 import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.model.HttpResponse
-import akka.http.scaladsl.model.MediaType
-import akka.http.scaladsl.model.ResponseEntity
-import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.Uri
-import akka.http.scaladsl.model.headers.Host
 import akka.http.scaladsl.model.ws.BinaryMessage
 import akka.http.scaladsl.model.ws.Message
 import akka.http.scaladsl.model.ws.UpgradeToWebSocket
+import akka.http.scaladsl.model.ws.WebSocketRequest
 import akka.http.scaladsl.model.ws.WebSocketUpgradeResponse
 import akka.stream.ActorMaterializer
 import akka.stream.ThrottleMode
@@ -43,122 +23,83 @@ import akka.stream.scaladsl.Keep
 import akka.stream.scaladsl.Sink
 import akka.stream.scaladsl.Source
 import akka.stream.scaladsl.TLSPlacebo
+import akka.testkit.TestKit
+import akka.util.ByteString
+import akka.util.ByteStringBuilder
 
+import org.scalatest.FlatSpecLike
+import org.scalatest.Matchers
+import org.scalatest.Suite
 import org.scalatest.concurrent.Eventually
-import org.scalatest.time.Millis
-import org.scalatest.time.Seconds
-import org.scalatest.time.Span
+import org.testng.annotations.Test
 
 
 class ServerLayerTest extends TestKit(ActorSystem()) with Suite with FlatSpecLike with Matchers with Eventually {
+
+  // Running with > 100.000 chunks this should not be able to complete before the initial delay elapses
+  // but it does which means the source is not backpressured!
+
+  /*
+   *    Source[Messages]   ->  wsClient  ->  wsServer  ->  Delay(50seconds)  ->  Sink.ignore
+   *
+   *    Sink.ignore        <-  wsClient  <-  wsServer  <-  Source.maybe
+   *
+   */
 
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
 
   val wsRequest = WebSocketRequest(Uri("http://narf.de/testUri"))
-  val httpHost = Host("narf.de")
 
-  // define test data  // expected time ~83 seconds | test took 85 seconds
   val chunkSizeByte = 1000
-  val testData = createTestData(8)
-  val delay = 10.millis
+  val testData = createTestData(100) // 100 Mb of data ~ 100.000 chunks
 
-  val counterMid = new AtomicInteger(0)
-  val counterStart = new AtomicInteger(0)
-  val counterEnd = new AtomicInteger(0)
 
   @Test(groups = Array("unit"))
   def testWithoutSockets(): Unit = {
     def httpServerFlow = Flow[HttpRequest].mapAsync(1)(handler)
 
-    val httpDataSink = Flow[HttpResponse]
-      .flatMapConcat(_.entity.dataBytes)
-      .map { data =>
-        counterEnd.incrementAndGet()
-        data
-      }
-      .toMat(Sink.reduce[ByteString]((a, b) => a.concat(b)))(Keep.right)
-
-    val httpMessageSource = Source.single {
-      val data = Source.fromIterator(() => testData.grouped(chunkSizeByte)).map { msg =>
-        counterStart.incrementAndGet()
-        msg
-      }
-
-      val reqEntity = HttpEntity(ContentTypes.`application/octet-stream`, data).withoutSizeLimit()
-      HttpRequest(method = HttpMethods.POST, entity = reqEntity)
-    }
-
-    // concat something that doesn't complete to ensure the in direction stays open
-    val stickyHttpMessageSource = httpMessageSource.concatMat(Source.maybe[HttpRequest])(Keep.right)
+    // wrap test data in ws objects and watch for complete
+    val wsMessageSource = Source.fromIterator(() => testData.grouped(chunkSizeByte))
+      .map(BinaryMessage(_))
+      .watchTermination()(Keep.right)
 
     // wire the server with the in and out source and run it
     val server = serverLayer.reversed.join(httpServerFlow)
-    val (senderPromise, doneReceiving) = stickyHttpMessageSource.viaMat(httpClientLayer.join(server))(Keep.left).toMat(httpDataSink)(Keep.both).run()
+    val senderFinished = wsMessageSource.viaMat(websocketClientLayer.join(server))(Keep.left).to(Sink.ignore).run()
 
+    val started = System.currentTimeMillis()
 
-
-
-
-
-
-    // await sending everything
-    eventually (timeout(Span(200, Seconds)), interval(Span(500, Millis))){
-      counterStart.get() shouldEqual testData.grouped(chunkSizeByte).length
+    senderFinished.onSuccess {
+      case _ => println(s"sending completed ${System.currentTimeMillis() - started}")
     }
-    println("Sending finished")
 
-    // await receiving everything
-    eventually (timeout(Span(200, Seconds)), interval(Span(500, Millis))){
-      counterEnd.get() shouldEqual testData.grouped(chunkSizeByte).length
-    }
-    println("Receiving finished")
-
-
-
-    // close & check data
-    senderPromise.isCompleted shouldEqual true // receiving of the response closes the connection
-    val result: ByteString = Await.result(doneReceiving, 1.seconds)
-
-    result.length shouldEqual testData.length
-    println(counterStart.get() + " " + counterMid.get() + " " + counterEnd.get())
-
-    // println("checking content")
-    // result shouldEqual testData
+    // block
+    Await.ready(senderFinished, 100.seconds)
+    Thread.sleep(1000)
   }
 
-  /* HttpResponse ~> +-------------+ ~> +------------+ ~> ByteString
-   *                 | serverLayer |    | sslPlacebo |
-   *  HttpRequest <~ +-------------+ <~ +------------+ <~ ByteString
-   */
+
   val serverLayer: BidiFlow[HttpResponse, ByteString, ByteString, HttpRequest, NotUsed] =
     Http().serverLayer.atop(TLSPlacebo())
 
-  val httpClientLayer: BidiFlow[HttpRequest, ByteString, ByteString, HttpResponse, NotUsed] =
-    Http().clientLayer(httpHost).atop(TLSPlacebo())
+  val websocketClientLayer: BidiFlow[Message, ByteString, ByteString, Message, Future[WebSocketUpgradeResponse]] =
+    Http().webSocketClientLayer(wsRequest).atop(TLSPlacebo())
+
 
   def handler(httpRequest: HttpRequest): Future[HttpResponse] = {
-    httpRequest match {
-      case request @ HttpRequest(HttpMethods.POST, uri: Uri, _, _, _) => handle(request)
-      case wrong: HttpRequest => {
-        Future.successful(HttpResponse(StatusCodes.MethodNotAllowed, entity = "Method not allowed."))
+    httpRequest.header[UpgradeToWebSocket] match {
+      case Some(upgrade) => {
+        val src = Source.maybe[Message]
+        // initial delay by 50 seconds -> create backpressure
+        val sink = Flow[Message].initialDelay(50.seconds).to(Sink.ignore)
+
+        Future.successful(upgrade.handleMessagesWithSinkSource(sink, src))
       }
+      case _ => throw new Exception()
     }
   }
 
-  def handle(request: HttpRequest): Future[HttpResponse] = {
-    // take input and throttle
-    val data = request.entity.dataBytes.map(msg => {
-      counterMid.incrementAndGet()
-      msg
-    }).via(delayMessage)
-
-    // wrap it back
-    val respEntity = HttpEntity(ContentTypes.`application/octet-stream`, data).withoutSizeLimit()
-    Future.successful(HttpResponse(entity = respEntity))
-  }
-
-  private def delayMessage = Flow[ByteString].throttle(1, delay, 10, ThrottleMode.shaping)
 
   private def createTestData(sizeInMb: Int): ByteString = {
     val mb1 = Array.fill(1024 * 1024) {
